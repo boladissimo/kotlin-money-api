2c8f9fefaa49d2a93569465a04a8abcf6d375615
Nomura Kenji <nomura.kenji@hotmail.com>
2021-06-02 21:17:36 -0300

    refactoring not found exception

--- src/main/kotlin/com/boladissimo/kotlinmoneyapi/stocks/service/StockServiceImpl.kt
+++ src/main/kotlin/com/boladissimo/kotlinmoneyapi/stocks/service/StockServiceImpl.kt
@@ -6,7 +6,9 @@ import com.boladissimo.kotlinmoneyapi.stocks.entity.DefaultStock
 import com.boladissimo.kotlinmoneyapi.stocks.entity.Stock
 import com.boladissimo.kotlinmoneyapi.stocks.mapper.StockMapper
 import com.boladissimo.kotlinmoneyapi.stocks.repository.StockRepository
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
+import org.springframework.web.server.ResponseStatusException
 
 @Service
 class StockServiceImpl(
@@ -27,22 +29,22 @@ class StockServiceImpl(
     }
 
     override fun update(code: String, stockRequest: StockUpdateRequest) {
-        if (stockRepository.existsById(code)) {
-            stockRepository.save(StockMapper(code, stockRequest.fantasyName))
-        } else {
-            throw java.lang.Exception() //TODO: 404 exception
-        }
+        validateStockExists(code)
+        stockRepository.save(StockMapper(code, stockRequest.fantasyName))
     }
 
     override fun delete(code: String) {
-        if (stockRepository.existsById(code)) {
-            stockRepository.deleteById(code)
-        } else {
-            throw java.lang.Exception() //TODO: 404 exception
-        }
+        validateStockExists(code)
+        stockRepository.deleteById(code)
     }
 
     private fun toEntity(mapper: StockMapper) : DefaultStock {
         return DefaultStock(mapper.code, mapper.fantasyName)
+    }
+
+    private fun validateStockExists(code: String) {
+        if (!stockRepository.existsById(code)) {
+            throw ResponseStatusException(HttpStatus.NOT_FOUND, "Stock with code $code not found")
+        }
     }
 }
